c31cac1b9493ce2a4aa168dfb411940c1acc876c
YousifHmada <yousifhmada.moos@gmail.com>
2021-03-21 15:48:11 +0200

    fix createIndex bug

--- server/app.js
+++ server/app.js
@@ -26,6 +26,7 @@ mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then(() => {
     logger.info('MongoDB connection succeeded');
