f5f09ed398a82b278f9d735c786109c862442954
YousifHmada <yousifhmada.moos@gmail.com>
2021-03-23 19:46:54 +0200

    test loadingCycle

---
+++ client/src/redux/helpers/loadingCycle.spec.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+import { expect } from 'chai';
+import createLoadingCycle, { REQUEST_STATUS } from './loadingCycle';
+
+describe('#createLoadingCycle', () => {
+  it('it should return loadinCycle initialState/reducers for the given resource name', () => {
+    const todosLoadingCycle = createLoadingCycle('todos');
+    expect(todosLoadingCycle.initialState).to.eql({
+      todosStatus: REQUEST_STATUS.IDLE,
+      todosError: undefined,
+    });
+    expect(Object.keys(todosLoadingCycle.reducers)).to.eql([
+      'todosIdle',
+      'todosLoading',
+      'todosSucceeded',
+      'todosFailed',
+    ]);
+  });
+});
